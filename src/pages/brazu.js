7f46c6f1e35388080dbdc6686538c3a8ab9d1fcd
Gabriel Carreira <gabriel.carreira1@gmail.com>
2024-05-28 01:50:10 -0300

    refactor brazu page

--- src/pages/brazu.js
+++ src/pages/brazu.js
@@ -1,20 +1,17 @@
-import { Button, Stack, Typography } from "@mui/joy";
-import { useRive, EventType, RiveEventType } from "@rive-app/react-canvas";
-import { useState, useEffect } from "react";
+import { Stack } from "@mui/joy";
+import { useRive } from "@rive-app/react-canvas";
 
 const Home = () => {
-  const { rive, RiveComponent } = useRive({
+  const { RiveComponent } = useRive({
     src: "brazu.riv",
     stateMachines: "State Machine 1",
     autoplay: true,
   });
 
   return (
-    <>
-      <Stack spacing={2}>
-        <RiveComponent style={{ height: "400px" }} />
-      </Stack>
-    </>
+    <Stack spacing={2}>
+      <RiveComponent style={{ height: "400px" }} />
+    </Stack>
   );
 };
 
